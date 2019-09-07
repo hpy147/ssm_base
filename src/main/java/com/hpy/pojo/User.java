7adb30d0acff674467d0f51ad53287026d49afc1
huangpeiyi <543268688@qq.com>
2019-09-07 19:17:51 +0800

    add common

--- src/main/java/com/hpy/pojo/User.java
+++ src/main/java/com/hpy/pojo/User.java
@@ -1,17 +1,19 @@
 package com.hpy.pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
 /**
  * Author: hpy
  * Description: 用户
  */
+@Getter@Setter
 public class User implements Serializable {
 
     private Integer id;
     private String username;
     private String password;
-
-    // getter、setter..
 
 }
