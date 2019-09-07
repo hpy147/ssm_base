c1c5000c02d194c028ba5294f64ab61e411e696b
huangpeiyi <543268688@qq.com>
2019-09-07 19:37:39 +0800

    modify ResponseCode.java

--- src/main/java/com/hpy/common/ResponseCode.java
+++ src/main/java/com/hpy/common/ResponseCode.java
@@ -13,7 +13,7 @@ public enum  ResponseCode {
     SUCCESS(0, "SUCCESS"),
     ERROR(1, "ERROR"),
     NEED_LOGIN(10, "NEED_LOGIN"),
-    ILLEGAL_ARGUMENT(3, "ILLEGAL_ARGUMENT");
+    ILLEGAL_ARGUMENT(30, "ILLEGAL_ARGUMENT");
 
     private Integer code;
     private String desc;
